6bd405e5f25a1726cd7a51c4732530b473239415
Kishlaya1999 <kishlaya54321@gmail.com>
2022-08-08 22:47:08 +0530

    Added the increase and decrease functionallity using props

--- src/Cart.js
+++ src/Cart.js
@@ -3,12 +3,103 @@ import CartItem from './CartItem';
 
 class Cart extends React.Component{
 
+     constructor(){
+          super();
+          this.state = {
+               products : [
+                    {
+                         price: 19999,
+                         title: 'Mobile Phone',
+                         qty : 5,
+                         img : ' ',
+                         id: 1
+                    },
+                    {
+                         price: 999,
+                         title: 'Watch',
+                         qty : 10,
+                         img : ' ',
+                         id: 2
+                    },
+                    {
+                         price: 49999,
+                         title: 'Laptop',
+                         qty : 1,
+                         img : ' ',
+                         id: 3
+                    }
+               ]
+          }
+     }
+
+     handleIncreaseQuantity = (product) => {
+          console.log("Please increase the quantity of ", product);
+          // Getting the product array from state
+          const {products} = this.state;
+
+          const indexOfClickedProduct = products.indexOf(product);
+
+          // increasing the value by 1 of clicked product in product array
+          products[indexOfClickedProduct].qty += 1;
+
+          // Updating the state with new values
+          this.setState({
+               // products: products
+               products  //shorthand of above statement
+          })
+
+          
+     }
+
+     handleDecreaseQuantity = (product) => {
+          console.log("Please increase the quantity of ", product);
+          // Getting the product array from state
+          const {products} = this.state;
+
+          const indexOfClickedProduct = products.indexOf(product);
+
+          if(products[indexOfClickedProduct].qty == 0){
+               return ;
+          }
+
+          // increasing the value by 1 of clicked product in product array
+          products[indexOfClickedProduct].qty -= 1;
+
+          // Updating the state with new values
+          this.setState({
+               // products: products
+               products  //shorthand of above statement
+          })
+
+          
+     }
+
      render(){
+          // const arr = [1,2,3,4,5];
+
+          const {products} = this.state;
+          
           return (
                <div className='cart'>
-                    <CartItem/>
-                    <CartItem/>
-                    <CartItem/>
+                    {/* Iterating throught the product array which is present in state and passing the data to CartItem */}
+                    {products.map((product)=>{
+                         return (
+                         <CartItem 
+                              product={product}
+                              key={product.id}
+                              onIncreaseQuantity={this.handleIncreaseQuantity}
+                              onDecreaseQuantity={this.handleDecreaseQuantity}
+                              />
+                         )
+                         // passing the handleIncreaseQuantity as a prop
+                    })}
+
+                    {/* <CartItem title={"Watch"} price={20000} qty={2}/> */}
+                    {/* <CartItem/>
+                    <CartItem/> */}
+               {/* { arr.map((item)=>{
+                    return item * 2;
+               }) } */}
                </div>
           );
      }
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,15 +2,7 @@ import React from 'react';
 
 
 class CartItem extends React.Component {
-     constructor(){
-          super();
-          this.state = {
-               price: 19999,
-               title: 'Mobile Phone',
-               qty : 1,
-               img : ' '
-          }
-     }
+     
 
      increaseQuantiy = () => {
           // this.state.qty += 1;\
@@ -84,27 +76,32 @@ class CartItem extends React.Component {
 
 
      render() {
+          // console.log(this.props);
+          // Destructuring the props
+          const {title , price, qty} = this.props.product;
           return ( 
+
                <div className = 'cart-item' >
                     <div className='left-block'>
                          <img style={styles.image} src="" alt="" />
                     </div>
                     <div className='right-block'>
-                         <div style={{fontSize: 25}}>{this.state.title}</div>
-                         <div style={{color:"#777"}}>Rs {this.state.price}</div>
-                         <div style={{color:"#777"}}>Qty : {this.state.qty}</div>
+                         <div style={{fontSize: 25}}>{title}</div>
+                         <div style={{color:"#777"}}>Rs {price}</div>
+                         <div style={{color:"#777"}}>Qty : {qty}</div>
                          <div className='cart-item-actions'>
                               {/* Buttons */}
                               <img 
                               className='action-icons' 
                               src="https://cdn-icons-png.flaticon.com/512/992/992651.png" 
                               alt="increase"
-                              onClick={this.increaseQuantiy} />
+                              onClick={() => this.props.onIncreaseQuantity(this.props.product)} />
+                              {/* On click we are calling the function which is executing the function which we are getting as prop and passing the current product as argument to handleIncreaseFunction */}
                               <img 
                               className='action-icons' 
                               src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
                               alt="decrease"
-                              onClick={this.decreasaeQuantiy} />
+                              onClick={() => this.props.onDecreaseQuantity(this.props.product)} />
                               <img 
                               className='action-icons' 
                               src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" 
